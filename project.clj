1db930e4389c846aceb0781d48c0002a51d1c9a7
Jesse Rosalia <jesse@rosalia.me>
2017-02-19 19:55:20 -0600

    changed com.shipliberty to org.shipliberty

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.shipliberty/lob-clj "0.1.0-SNAPSHOT"
+(defproject org.shipliberty/lob-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
