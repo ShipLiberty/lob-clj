46ffed793e98b50c7e80fe811ee9ac011a0c0b1d
Jesse Rosalia <jesse@rosalia.me>
2017-02-19 20:07:14 -0600

    Moved api version call to inside make-client, so it doesn't get called when uberjaring

--- src/lob_clj/core.clj
+++ src/lob_clj/core.clj
@@ -2,8 +2,7 @@
   (:import [com.lob Lob]
            [com.lob.client AsyncLobClient, LobClient]))
 
-;; Precaution, and also lets us test with newer versions (by varying the string)
-(Lob/setApiVersion "2016-06-30")
-
 (defn make-client [^String api-key]
+  ;; Precaution, and also lets us test with newer versions (by varying the string)
+  (Lob/setApiVersion "2016-06-30")
   (. AsyncLobClient (createDefault api-key)))
